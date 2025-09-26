470433e7bb8d31acc0db1c83ef85d48ba0538fb2
slash7it <kwmcwilliams@gmail.com>
2025-09-25 21:40:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -82,10 +82,18 @@ if (ani) {
     renderer.render( scene, camera );
 }
 
+function log(type, data) {
+    var ul = document.getElementById("console");
+    var li = document.createElement("li");
+    var payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
+    li.appendChild(payload)
+    ul.prepend(li);
+}
+
 var app = new window.Webex.Application();
 
 app.onReady().then(function () {
-  console.log('App is ready. App info:', app);
+  log('App is ready. App info:', app);
 });
 
 document.onclick = (e) => {
